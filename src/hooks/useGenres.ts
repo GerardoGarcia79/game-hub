76489d58607164b90392c318a1329e175ecf9f0e
Gerardo de Leon <gerardo-leon10@hotmail.com>
2024-05-24 14:40:50 -0600

    Refactoring- fetch games with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import axios from "axios";
 import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 
 export interface Genre {
   id: number;
